7cb4cd02bc812cfef734d169b8ebe60d6b41f7bd
alexander ekstrand <aekstrand92@gmail.com>
2022-07-13 11:27:55 +0200

    Add solution

---
+++ SwordleHelper/Program.cs
@@ -0,0 +1,19 @@
+﻿using SwordleHelper;
+
+var wordsList = Words.Get;
+Console.WriteLine("What letters is not in the word?");
+var lettersNotInWordInput = Console.ReadLine();
+Console.WriteLine("What letters is in the word?");
+var lettersInWordInput = Console.ReadLine();
+
+var lettersNotInWord = !string.IsNullOrEmpty(lettersNotInWordInput) ? lettersNotInWordInput.ToCharArray() : Array.Empty<char>();
+var lettersInWord = !string.IsNullOrEmpty(lettersInWordInput) ? lettersInWordInput.ToCharArray() : Array.Empty<char>();
+
+wordsList = lettersInWord.Aggregate(wordsList, (current, letterInWord) => current.Where(w => w.Contains(letterInWord)).ToArray());
+wordsList = lettersNotInWord.Aggregate(wordsList, (current, letterNotInWord) => current.Where(w => !w.Contains(letterNotInWord)).ToArray());
+
+Console.WriteLine("Suggestions: ");
+foreach (var word in wordsList)
+    Console.WriteLine(word);
+
+Console.ReadLine();
